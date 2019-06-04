bcdceb13bbc83e134e08a5b18ed0b655da969745
sharicodes <srosenb@yahoo.com>
2019-06-04 13:43:36 -0400

    inital

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => console.log(position),
+//     err => console.log(err)
+//   );
+//
+//   return <div>Latitude: </div>;
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   //super is a reference to the parent's constructor function
+  //   //this is the ONLY TIME we do direct assisnment
+  //   //to this.state.
+  //   this.state = { lat: null, errorMessage: " " };
+  //   //we dont know what latitude is yet so default to null
+  // }
+
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+  //we called setState!!
+
+  //we did not directly assign state object- only done upon initialization
+
+  // componentDidUpdate() {
+  //   console.log("My component was just updated - it rerendered!");
+  // }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+  //React says we have to define render!!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
